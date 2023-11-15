0f10edc1df6ca4f771ceb473a9f24d5d76a118b6
Luka Nikolac <122328343+lnikol00@users.noreply.github.com>
2023-11-15 13:38:46 +0100

    adding Airport Model

---
+++ AirportApplication/AirportApplication/Models/Domain/Airport.cs
@@ -0,0 +1,27 @@
+﻿namespace AirportApplication.Models.Domain
+{
+    public class Airport
+    {
+        // Unique ID for each plane
+        public int Id { get; set; }
+
+        // Plane model (name)
+        public string? Model { get; set; }
+
+        // Year plane was built
+        public string? Year { get; set; }
+
+        // Country origin (Country where plane was built)
+        public string? Country { get; set; }
+
+        // Number of passengers available on flight
+        public int Capacity { get; set; }
+
+        // List of cities to which the plane travels 
+        // Example: Dubrovnik-Zagreb, Zagreb-London ...
+        public List<string>? Routes { get; set; }
+
+        // List of crew members currently aboard flight
+        public List<string>? Crew { get; set; }
+    }
+}
