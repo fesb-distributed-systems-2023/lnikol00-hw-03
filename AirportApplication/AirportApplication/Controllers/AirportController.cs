a02a6a9a7e1ed6b098ccf73d6c5c8d09651bc3c5
Luka Nikolac <122328343+lnikol00@users.noreply.github.com>
2023-11-16 18:53:18 +0100

    adding AirportController

---
+++ AirportApplication/AirportApplication/Controllers/AirportController.cs
@@ -0,0 +1,54 @@
+﻿/*
+**********************************
+* Author: Luka Nikolac
+* Project Task: Airport - Phase 2
+**********************************
+* Description:
+* 
+*    Create - Add new plane
+*    Read - Get all planes
+*    Read - Get specific plane
+*    Delete - Delete plane
+*
+**********************************
+*/
+
+using AirportApplication.Models.Domain;
+using AirportApplication.Repositories;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AirportApplication.Controllers
+{
+    [ApiController]
+    public class AirportController : ControllerBase
+    {
+        private readonly AirportRepository _airportRepository;
+
+        public AirportController(AirportRepository airportRepository)
+        {
+            _airportRepository = airportRepository;
+        }
+
+        // CREATE
+        // Postman request: POST http://localhost:5079/planes/new
+        // Postman response: New plane created!
+        [HttpPost("/planes/new")]
+        public IActionResult CreateNewEmail([FromBody] Airport plane) 
+        {
+            bool fSuccess = _airportRepository.CreateNewPlane(plane);
+
+            if(fSuccess)
+            {
+                return Ok("New plane created!");
+            }
+            else
+            {
+                return BadRequest("Something went wrong!");
+            }
+        }
+
+        // GET ALL
+
+    }
+}
