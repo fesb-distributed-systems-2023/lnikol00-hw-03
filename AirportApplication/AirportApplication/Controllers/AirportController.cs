a9dd19f38d54ba068631f10530c489b08703c84d
Luka Nikolac <122328343+lnikol00@users.noreply.github.com>
2023-11-17 01:44:24 +0100

    update AirportController,  get all, get single and delete with comments

--- AirportApplication/AirportApplication/Controllers/AirportController.cs
+++ AirportApplication/AirportApplication/Controllers/AirportController.cs
@@ -5,10 +5,10 @@
 **********************************
 * Description:
 * 
-*    Create - Add new plane
-*    Read - Get all planes
-*    Read - Get specific plane
-*    Delete - Delete plane
+*    CREATE - Add new plane
+*    READ - Get all planes
+*    READ - Get specific plane
+*    DELETE - Delete plane
 *
 **********************************
 */
@@ -33,6 +33,17 @@ namespace AirportApplication.Controllers
         // CREATE
         // Postman request: POST http://localhost:5079/planes/new
         // Postman response: New plane created!
+        /* Postman body:
+            * {
+                 "id": 1,
+                 "model":"Plane 1",
+                 "year":"2000",
+                 "country": "Croatia",
+                 "capacity": 2000,
+                 "routes":["Miami", "New York", "Washington DC"],
+                 "crew":["Andrew", "Floyd", "Stephanie"]
+               }           
+         */
         [HttpPost("/planes/new")]
         public IActionResult CreateNewEmail([FromBody] Airport plane) 
         {
@@ -49,6 +60,107 @@ namespace AirportApplication.Controllers
         }
 
         // GET ALL
+        // Postman request: GET http://localhost:5079/planes/all
+        /* Postman response : 
+         * [
+                {
+                   "id": 1,
+                   "model": "Plane 1",
+                   "year": "2000",
+                   "country": "Croatia",
+                   "capacity": 2000,
+                   "routes": [
+                        "Miami",
+                        "New York",
+                        "Washington DC"
+                   ],
+                   "crew": [
+                       "Andrew",
+                       "Floyd",
+                       "Stephanie"
+                    ]
+                },
+                {
+                    "id": 2,
+                    "model": "Plane 2",
+                    "year": "2001",
+                    "country": "America",
+                    "capacity": 1800,
+                    "routes": [
+                        "Bla",
+                        "Bla",
+                        "Blab DC"
+                    ],
+                    "crew": [
+                        "One",
+                        "Two",
+                        "Three"
+                    ]
+                }
+        
+           ]
+        */
+        [HttpGet("/planes/all")]
+        public IActionResult GetAllPlanes()
+        {
+            return Ok(_airportRepository.GetAllPlanes());
+        }
 
+        // GET SINGLE PLANE
+        // Postman request (success): GET http://localhost:5079/planes/2
+        // Postman response (success): 
+        /* 
+           {
+                "id": 2,
+                "model": "Plane 2",
+                "year": "2001",
+                "country": "America",
+                "capacity": 1800,
+                "routes": [
+                    "Bla",
+                    "Bla",
+                    "Blab DC"
+                ],
+                "crew": [
+                    "One",
+                    "Two",
+                    "Three"
+                ]
+            }
+         */
+        // Postman request (error): GET http://localhost:5079/planes/12
+        // Postman response (error): Plane with id:12 doesn't exist!
+        [HttpGet("/planes/{id}")]
+        public IActionResult GetSinglePlane([FromRoute]int id)
+        {
+            var plane = _airportRepository.GetSinglePlane(id);
+
+            if(plane is null)
+            {
+                return NotFound($"Plane with id:{id} doesn't exist!");
+            }
+            else
+            {
+                return Ok(plane);
+            }
+        }
+
+        // DELETE
+        // Postman request (success): DELETE http://localhost:5079/planes/2
+        // Postman response (success): Deleted plane with id=2!
+        // Postman request (error): DELETE http://localhost:5079/planes/12
+        // Postman response (error): Could not find plane with id=12!
+        [HttpDelete("/planes/{id}")]
+        public IActionResult DeletePlane([FromRoute]int id)
+        {
+            if(_airportRepository.DeletePlane(id))
+            {
+                return Ok($"Deleted plane with id={id}!");
+            }
+            else
+            {
+                return NotFound($"Could not find plane with id={id}!");
+            }
+        }
     }
 }
